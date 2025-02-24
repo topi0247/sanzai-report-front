9461e8ebd715f2b167139c9d606d41eafe1f8684
topi0247 <karubona.ra247@gmail.com>
2025-02-24 15:30:12 +0900

    fix: ナビゲーションメニューのリンク修正

--- src/components/header-menu.tsx
+++ src/components/header-menu.tsx
@@ -1,35 +1,37 @@
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-  } from "@/components/ui/navigation-menu";
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+
 import { Separator } from "@/components/ui/separator";
 import { NavLink } from "react-router";
 
 export const HeaderMenu = () => {
-    return (
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>メニュー</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>
-                <NavLink to="#" end>
-                  hoge
-                </NavLink>
-              </NavigationMenuLink>
-              <Separator />
-              <NavigationMenuLink>
-                <NavLink to="#" end>
-                  hoge
-                </NavLink>
-              </NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    );
-  };
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>メニュー</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <NavLink to="#" end>
+                昔の散財
+              </NavLink>
+            </NavigationMenuLink>
+            <Separator />
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <NavLink to="#" end>
+                散財グラフ
+              </NavLink>
+            </NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
